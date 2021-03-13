cedbfe4e619dd5517925753393704400d6ffd778
Mikhail Blazhko <michelin8585@gmail.com>
2021-03-13 14:25:00 +0200

    feat: dream team task handle

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let result = 0;
+  matrix.forEach(element => {
+    result += element.reduce( (sum, num) => {
+      return sum + Number(num === '^^');
+    },0);
+  });
+  return result;
 };
