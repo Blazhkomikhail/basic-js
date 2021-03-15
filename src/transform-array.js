031884c629709dd58f583619d95db712afaddde9
Mikhail Blazhko <michelin8585@gmail.com>
2021-03-15 11:47:51 +0200

    feat: solve transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(matrix) {
+  if ( !Array.isArray(matrix) ) new CustomError('Not implemented');
+  if ( !matrix.length ) return matrix;
+  const copy = [...matrix];
+  let targetInd;
+    for (let i = 0; i < copy.length; i++) {
+      if (copy[i] === '--discard-next') {
+        if (i !== copy.length - 1) {
+          targetInd = i + 1;
+          copy.splice(i, 2);
+          i -= 2;
+        } else {
+          copy.splice(i, 1);
+          i -= 1;
+        }  
+      } else if (copy[i] === '--discard-prev') {
+        if (i !== 0 && targetInd - i !== 1) {
+          copy.splice(i - 1, 2);
+          i -= 2;
+        } else {
+          copy.splice(i, 1);
+          i -= 1;
+        }
+      } else if (copy[i] === '--double-next') {
+        if (i !== copy.length - 1) {
+          copy.splice(i, 1, copy[i + 1]);
+        } else {
+          copy.splice(i, 1);
+        }
+      } else if (copy[i] === '--double-prev') {
+        if (i !== 0 && targetInd - i !== 1) {
+          copy.splice(i, 1, copy[i - 1]);
+        } else {
+          copy.splice(i, 1);
+          i -= 1;
+        }
+      } 
+    }
+  return copy;
+}
